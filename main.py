4349d0dca5aa08a35b8e1d64d05347a6c5e8c327
RohSungKyun <songkyun1108@naver.com>
2022-09-22 16:03:34 +0900

    2022-09-22 실습과제#4 3번

---
+++ main.py
@@ -0,0 +1,10 @@
+
+print("이름을 입력하세요:", end=' ')
+name = input()
+print("학번을 입력하세요:", end=' ')
+student_num = int(input())
+print()
+
+print("<출력>")
+print("이름: %s"%(name))
+print("학번: %d"%(student_num))
