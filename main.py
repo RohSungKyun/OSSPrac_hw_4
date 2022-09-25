96ca000a7545406f69ba0d33137695765994dd75
RohSungKyun <songkyun1108@naver.com>
2022-09-25 19:27:30 +0900

    2022-09-25 실습과제#4 5번

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 
+from os import major
+
+
 print("이름을 입력하세요:", end=' ')
 name = input()
 print("학번을 입력하세요:", end=' ')
 student_num = int(input())
+print("학과를 입력하세요:", end=' ')
+department = input()
 print()
 
 print("<출력>")
 print("이름: %s"%(name))
 print("학번: %d"%(student_num))
+print("학과: %s"%(department))
